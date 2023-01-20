de7d4ca33b57ef48acf2f66b3ba57ce228bbf15f
MRX <janybekmma30@gmail.com>
2023-01-20 20:44:36 +0600

    f

---
+++ ifelif.py
@@ -0,0 +1,79 @@
+
+'''
+print("аквапарк")
+age = int(input("you age? "))
+if age  == 5:
+	print("вход беслпатный")
+	if age ==4:
+		print("тебе 4. Нельзя")
+elif age >5 or age <18:
+	print('вход стоит 5 сом ')
+elif age > 18:
+	print('вашт билет стоит 40 сом ')
+'''
+'''
+a = 2**3
+b = 3**2
+if a > b :
+	print('more a ')
+if b > a:
+	print('correct B ') 
+''' 
+# 3
+'''
+a = int(input('napishi chislo'))
+if a % 2 == 0:
+	print('chetnoe')
+else:
+	 pint('nechetnoe')
+if a % 3 == 0:
+	print('delitsa bezastatka')
+else:
+	print('s astatkom')
+if a **  2 > 1000:
+	print('a bolshe 1000')
+else:
+	proint('a menshe 1000')
+'''
+#5
+'''
+a = 10//5 
+b = 10/5
+if a == b:
+	print(a+b)
+else:
+	print('neravno')
+'''
+#6
+'''
+
+a = int(input)
+if a < 0:
+	print('otritsatelnoe cislo')
+else:
+	print('polojetelniy')
+'''
+#7
+'''	
+a = 10
+b = 17
+if a:
+	print(a)
+'''
+'''
+a = 10 
+b = 5 
+if a and b:
+	print(bool(a>0),bool(b>0))
+'''
+'''
+a = 10
+if a :
+	print(true)
+'''
+x = 13
+if x**2<172:
+	c = x**2
+	if c**2 >172:
+		print(true)
+
---
+++ lessons3/list_tuple.py
@@ -0,0 +1,13 @@
+#0
+# a = [('ermek'),('vlad'),('nooruz'),('baytik'),('janybek')]
+# print(a)
+#1
+# a = ('ermek,jake,aiba')
+# print(a[:])
+# 2
+# a = [1,'umar',True,('beka'),1.5]
+# print(a)
+
+list1 = ['azza','tre','tyhg','poin','kopp']
+st1 = ''.join(list1)
+print(st1) 
